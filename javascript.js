c4cc1c7b3664b12acd0e722f69bcaa47fc81de3f
Soso16-collab <240217108+Soso16-collab@users.noreply.github.com>
2026-02-02 13:17:20 -0500

    Add logic for the computer choice
    Added a function that will generate a number and return a string based on its value: rock, paper, or scissors.

--- javascript.js
+++ javascript.js
@@ -1,1 +1,23 @@
-console.log("Hello World!")
+/* 
+generate value
+convert value to string
+return string
+*/
+
+getComputerChoice()
+
+function getComputerChoice() {
+    let computerChoice = Math.random()
+    if (computerChoice < 1/3) {
+        console.log(`rock is the computer's choice.`)
+        return "rock"
+    }
+    else if (computerChoice < 2/3) {
+        console.log(`paper is the computer's choice.`)
+        return "paper"
+    }
+    else {
+        console.log(`scissors is the computer's choice.`)
+        return "scissors"
+    }
+}
