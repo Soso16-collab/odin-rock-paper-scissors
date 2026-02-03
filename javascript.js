eed1594095f2966675a54ddd12dcef495eb505fb
Soso16-collab <240217108+Soso16-collab@users.noreply.github.com>
2026-02-03 14:01:06 -0500

    Add human choice logic and logic to play a round
    Added a function that will prompt the player for their choice, and another function that compares the player's and computer's choices to find the winner.

--- javascript.js
+++ javascript.js
@@ -1,23 +1,51 @@
-/* 
-generate value
-convert value to string
-return string
-*/
+let humanScore = 0
+let computerScore = 0
 
-getComputerChoice()
+playRound(getHumanChoice(), getComputerChoice())
 
 function getComputerChoice() {
     let computerChoice = Math.random()
     if (computerChoice < 1/3) {
-        console.log(`rock is the computer's choice.`)
-        return "rock"
+        console.log(`ROCK is the computer's choice.`) //debug
+        return "ROCK"
     }
     else if (computerChoice < 2/3) {
-        console.log(`paper is the computer's choice.`)
-        return "paper"
+        console.log(`PAPER is the computer's choice.`) //debug
+        return "PAPER"
     }
     else {
-        console.log(`scissors is the computer's choice.`)
-        return "scissors"
+        console.log(`SCISSORS is the computer's choice.`) //debuggit 
+        return "SCISSORS"
     }
 }
+
+function getHumanChoice() {
+    let humanChoice = prompt("Rock, Paper, or Scissors?")
+    if (humanChoice !== null) {
+    console.log(humanChoice.toUpperCase())
+    return humanChoice.toUpperCase() 
+    }
+}
+
+function playRound(humanChoice, computerChoice) {
+    // Tie
+    if (humanChoice === computerChoice) {
+        console.log(`You tie! ${humanChoice} is equal to ${computerChoice}`)
+    }
+    // Win
+    else if (humanChoice === "ROCK" && computerChoice === "SCISSORS" || humanChoice === "PAPER" && computerChoice === "ROCK" || humanChoice === "SCISSORS" && computerChoice === "PAPER") {
+        console.log(`You win! ${humanChoice} beats ${computerChoice}`)
+    }
+    // Lose
+    else if (humanChoice === "ROCK" && computerChoice === "PAPER" || humanChoice === "PAPER" && computerChoice === "SCISSORS" || humanChoice === "SCISSORS" && computerChoice === "ROCK") {
+        console.log(`You lost! ${computerChoice} beats ${humanChoice}`)
+    }
+}
+
+/* 
+find out who won COMPLETED
+    tie COMPLETED
+    win/lose COMPLETED
+log the results COMPLETED
+increment score based on who won
+*/
