d030c7aec74f439d51ca4134905b99416a4f376a
Soso16-collab <240217108+Soso16-collab@users.noreply.github.com>
2026-02-03 15:21:07 -0500

    Write logic to play multiple rounds and a score system
    Created a new function that plays 5 rounds and tallies up a score to declare it at the end.

--- javascript.js
+++ javascript.js
@@ -1,51 +1,72 @@
-let humanScore = 0
-let computerScore = 0
-
-playRound(getHumanChoice(), getComputerChoice())
+playGame()
 
 function getComputerChoice() {
     let computerChoice = Math.random()
     if (computerChoice < 1/3) {
-        console.log(`ROCK is the computer's choice.`) //debug
         return "ROCK"
     }
     else if (computerChoice < 2/3) {
-        console.log(`PAPER is the computer's choice.`) //debug
         return "PAPER"
     }
     else {
-        console.log(`SCISSORS is the computer's choice.`) //debuggit 
         return "SCISSORS"
     }
 }
 
 function getHumanChoice() {
     let humanChoice = prompt("Rock, Paper, or Scissors?")
-    if (humanChoice !== null) {
-    console.log(humanChoice.toUpperCase())
-    return humanChoice.toUpperCase() 
-    }
+    return humanChoice
 }
 
 function playRound(humanChoice, computerChoice) {
+    if (humanChoice === null || humanChoice === undefined) { // This will occur if the user presses "cancel" on the prompt.
+        return "CANCEL GAME"
+    }
+    humanChoice = humanChoice.toUpperCase() 
     // Tie
     if (humanChoice === computerChoice) {
         console.log(`You tie! ${humanChoice} is equal to ${computerChoice}`)
+        return "ROUND TIED"
     }
     // Win
-    else if (humanChoice === "ROCK" && computerChoice === "SCISSORS" || humanChoice === "PAPER" && computerChoice === "ROCK" || humanChoice === "SCISSORS" && computerChoice === "PAPER") {
+    else if (
+        humanChoice === "ROCK" && computerChoice === "SCISSORS" || 
+        humanChoice === "PAPER" && computerChoice === "ROCK" || 
+        humanChoice === "SCISSORS" && computerChoice === "PAPER") {
         console.log(`You win! ${humanChoice} beats ${computerChoice}`)
+        return "HUMAN WON"
     }
-    // Lose
-    else if (humanChoice === "ROCK" && computerChoice === "PAPER" || humanChoice === "PAPER" && computerChoice === "SCISSORS" || humanChoice === "SCISSORS" && computerChoice === "ROCK") {
+    // Lost
+    else if (
+        humanChoice === "ROCK" && computerChoice === "PAPER" || 
+        humanChoice === "PAPER" && computerChoice === "SCISSORS" || 
+        humanChoice === "SCISSORS" && computerChoice === "ROCK") {
         console.log(`You lost! ${computerChoice} beats ${humanChoice}`)
+        return "COMPUTER WON"
     }
 }
 
-/* 
-find out who won COMPLETED
-    tie COMPLETED
-    win/lose COMPLETED
-log the results COMPLETED
-increment score based on who won
-*/
+function playGame() {
+let humanScore = 0
+let computerScore = 0
+for (let i = 0; i < 5; i++) {
+    let roundResult = playRound(getHumanChoice(), getComputerChoice()) 
+    if (roundResult === "HUMAN WON") {
+    humanScore++
+}
+    else if (roundResult === "COMPUTER WON") {
+    computerScore++
+}
+    else if (roundResult === "CANCEL GAME") {
+        return
+    }
+console.log(`Your score is ${humanScore}. The computer's score is ${computerScore}.`)
+}
+console.log(`You have ${
+    humanScore > computerScore
+    ? "won"
+    : humanScore < computerScore
+    ? "lost"
+    : "tied"
+    } the game by a score of ${humanScore} to ${computerScore}!`)
+}
